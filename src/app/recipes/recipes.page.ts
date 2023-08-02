fec63dacd03f43dc3b7efdfdc01d383447138d7d
Salim <bouzidsezlim@gmail.com>
2023-08-02 10:26:13 +0200

    first commit

---
+++ src/app/recipes/recipes.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { RecipesService } from './recipes.service';
+
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.page.html',
+  styleUrls: ['./recipes.page.scss'],
+})
+export class RecipesPage implements OnInit {
+
+
+  recipes :Recipe[] = [];  
+  
+  constructor (private recipeService :RecipesService) { }
+
+  ngOnInit() {
+  //console.log('refresh recipes')
+  //this.recipes = this.recipeService.getAllRecipes();
+  }
+  ionViewWillEnter(){
+    this.recipes = this.recipeService.getAllRecipes();
+   
+       }
+ 
+}
+
+
+
